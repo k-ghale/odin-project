4e65a3bc34f7729e7056d12a2a60170488eb2c73
k-ghale <kabinghale888@gmail.com>
2026-05-02 03:53:40 -0400

    add: added refresher

---
+++ js/refresher.js
@@ -0,0 +1,14 @@
+
+const myObject = {
+    property: "value",
+    otherProperty: 77,
+    "obnoxious property": function(){
+        //do stuff
+    },
+}
+
+//dotnotation
+console.log(myObject.property)
+
+//bracket notation
+console.log(myObject["obnoxious property"])
